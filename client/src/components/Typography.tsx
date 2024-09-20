ed8e2e15ec36b98762c7d5e933e8460e3f063d57
vtipo_sh_nik <akzhol.kasym2004@mail.ru>
2024-09-20 18:24:03 +0600

    typography util comps added

--- client/src/components/Typography.tsx
+++ client/src/components/Typography.tsx
@@ -7,13 +7,23 @@ interface ITypography {
 }
 
 export function Header1({ children, styles = '' }: ITypography) {
-    return <h1 className={`${styles} text-[40px] font-[700]`}> {children} </h1>
+    return <h1 className={`${styles} text-[24px] md:text-[40px] font-[700]`}> {children} </h1>
 }
 
 export function Paragraph({ children, styles = '' }: ITypography) {
-    return <p className={`${styles} text-[20px] font-[300]`}>{children}</p>
+    return <p className={`text-[20px] font-[300] ${styles} `}>{children}</p>
 }
 
 export function PinkLinkBtn({ children, styles = '', path }: ITypography) {
-    return <Link className={`${styles} bg-[#E4D9FF] text-[24px] font-[700] w-[160px] rounded-[100px] p- text-center`} to={path || '#'}>{children}</Link>
+    return <Link className={`${styles} bg-[#E4D9FF] text-[24px] font-[700] w-[180px] rounded-[100px] text-center `} to={path || '#'}>{children}</Link>
+}
+
+export const TruncateText = ({ text, maxLength }: { text: string, maxLength: number }) => {
+    if (text.length < maxLength) return <span>{text}</span>
+
+    return (
+        <span>
+            {text.slice(0, maxLength)}...
+        </span>
+    )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import BlackBgCard from "../components/Home/BlackBgCard";
+import Categories from "../components/Home/Categories";
+import Hero from "../components/Home/Hero";
+import Reviews from "../components/Home/Reviews";
+
 export default function Home() {
     return (
-        <section>
-            Home
+        <section className="flex flex-col">
+            <Hero />
+            <Categories />
+            <BlackBgCard />
+            <Reviews />
         </section>
     )
 };
